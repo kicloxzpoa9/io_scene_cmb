bc0a73d4102d9d1c93d75bd4ae3a7a1083e7ac23
Meumano <95933636+kicloxzpoa9@users.noreply.github.com>
2026-02-20 23:23:29 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,3 @@
-bl_info = {
-    "name":         "CMB Import",
-    "author":       "M-1 (Discord: M-1#1972)",
-    "blender":      (2, 79, 0),
-    "version":      (1,0,0),
-    "location":     "File > Import",
-    "warning":      "This add-on is an early release and bugs may occur",
-    "description":  "Import Grezzo's \"Ctr Model Binary\" file(s)",
-    "category":     "Import-Export",
-    "wiki_url":     "",
-    "tracker_url":  "",
-}
-
 import os, bpy
 from bpy.props import *
 from bpy_extras.io_utils import ImportHelper
